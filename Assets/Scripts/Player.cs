5ecdf63faf640e821dd620a2b52460ecc15cafdf
harshhh-dev <harshhh.singh.personal@gmail.com>
2020-08-21 20:07:09 -0400

    added moving, and shooting.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] float moveSpeed;
+
+    public Rigidbody2D rb;
+    Vector2 moveVelocity;
+
+    private void Update()
+    {
+        Vector2 movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = movementInput.normalized * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
